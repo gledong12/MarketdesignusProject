cdc83339acf70628ccbadc7192f4b256362e7a73
gledong12 <eagle5424228@gmail.com>
2021-06-25 18:49:28 +0900

    ADD Models.py  - Moving_Company_Information  - Car  - Company_Car  - Application_of_Moving  - Moving_type  - Satisfaction  - Customer_Feedback_History

---
+++ moving/information/models.py
@@ -0,0 +1,77 @@
+from django.db import models
+
+class Moving_Company_Information(models.Model):
+    name                       = models.CharField(max_length=50)
+    master                     = models.CharField(max_length=20)
+    phone_number               = models.CharField(max_length=10)
+    address                    = models.CharField(max_length=100)
+    business_number            = models.CharField(max_length=20)
+    business_registration_date = models.DateField()
+    staff                      = models.IntegerField()
+    matching                   = models.BooleanField(default=False)
+    number_of_car              = models.ManyToManyField('Car', through='Company_Car', related_name='number_of_car')
+    
+    class Meta:
+        db_table = 'company_infomation'
+        
+class Car(models.Model):
+    name = models.CharField(max_length=30)
+    
+    class Meta:
+        db_table = 'car'
+        
+class Company_Car(models.Model):
+    company = models.ForeignKey('Moving_Company_Information', on_delete=models.CASCADE, related_name='company')
+    car     = models.ForeignKey('Car', on_delete=models.CASCADE, related_name='car')
+    number  = models.IntegerField()
+    
+    class Meta:
+        db_table = 'company_car'
+        
+        
+class Application_of_Moving(models.Model):
+    name = models.CharField(max_length=45)
+    phone_number = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+    departure_point = models.CharField(max_length=100)
+    departure_floor = models.IntegerField()
+    destination_point = models.CharField(max_length=100)
+    destination_floor = models.IntegerField()
+    moving_date = models.DateField()
+    storaging_moving = models.BooleanField(default=False)
+    terms_of_use = models.BooleanField(default=False)
+    personal_information = models.BooleanField(default=False)
+    marketing_information = models.BooleanField(default=False)
+    
+    class Meta:
+        db_table = 'application_of_moving'
+        
+class Moving_type(models.Model):
+    name = models.CharField(max_length=50)
+    
+    class Meta:
+        db_table = 'moving_type'
+        
+class Satisfaction(models.Model):
+    name = models.CharField(max_length=20)
+    
+    class Meta:
+        db_table = 'satisfaction'
+        
+class Customer_Feedback_History(models.Model):
+    opening_information       = models.BooleanField(default=False)
+    revisit                   = models.BooleanField(default=False)
+    price                     = models.IntegerField()
+    feeadback_date            = models.DateField(auto_now_add=True)
+    feedback                  = models.TextField()
+    customer_information      = models.ForeignKey('Moving_Company_information', on_delete=models.CASCADE, related_name='customer_information')
+    company_information       = models.ForeignKey('Application_of_Moving', on_delete=models.CASCADE, related_name='company_information')
+    moving_type               = models.ForeignKey('Moving_type', on_delete=models.CASCADE, related_name='moving_type')
+    professional_satisfaction = models.ForeignKey('Satisfaction', on_delete=models.CASCADE, related_name='professional_satisfaction')
+    price_satisfaction        = models.ForeignKey('Satisfaction', on_delete=models.CASCADE, related_name='price_satisfaction')
+    kindness_satisfaction     = models.ForeignKey('Satisfaction', on_delete=models.CASCADE, related_name='Kindnedd_satisfaction')
+    
+    class Meta:
+        db_table = 'feedback_history'
+    
+    
---
+++ moving/information/views.py
@@ -0,0 +1,2 @@
+class mainview():
+    pass
