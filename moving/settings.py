dcb9625d6ce39a9bbc6a9092edf38d29121210a1
gledong12 <eagle5424228@gmail.com>
2021-06-30 17:31:22 +0900

    MODIFY: circle ci settings.py

--- moving/settings.py
+++ moving/settings.py
@@ -1,5 +1,4 @@
 import os
-import my_settings
 
 from pathlib import Path
 
@@ -64,6 +63,21 @@ if 'DJANGO_DB_USERNAME' in os.environ:
         'HOST'    : os.environ['DJANGO_DB_HOST'],
         'PORT'    : os.environ['DJANGO_DB_PORT']
     }}
+elif 'MYSQL_DATABASE' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ["DB_NAME"],
+            'USER': os.environ["DB_USER"],
+            'PASSWORD': os.environ["DB_PASSWORD"],
+            'HOST': os.environ["DB_HOST"],
+            'PORT': os.environ["DB_PORT"],
+            'TEST': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': 'test.db',
+            }
+        }
+    }
 else:   
     DATABASES = {
     'default': {
